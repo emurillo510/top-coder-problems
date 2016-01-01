2e57d02065337c6995f327dc43b757e6c259c733
Elton Murillo <emurillo510@gmail.com>
2015-12-31 19:13:40 -0800

    Adds requirements for ZigZag

--- BadNeighbors.java
+++ BadNeighbors.java
@@ -31,6 +31,32 @@ Constraints
 -	donations contains between 2 and 40 elements, inclusive.
 -	Each element in donations is between 1 and 1000, inclusive.
 
+Examples
+0)  
+      
+ { 10, 3, 2, 5, 7, 8 }
+Returns: 19
+The maximum donation is 19, achieved by 10+2+7. It would be better to take 10+5+8 except that the 10 and 8 donations are from neighbors.
+1)  
+      
+{ 11, 15 }
+Returns: 15
+2)  
+      
+{ 7, 7, 7, 7, 7, 7, 7 }
+Returns: 21
+3)  
+      
+{ 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 }
+Returns: 16
+4)  
+      
+{ 94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61,  
+  6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
+  52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72 }
+Returns: 2926
+
+
 */
 public class BadNeighbors{
 
